e036d045a04779ad88f9e11aeb9f0b3d60f6deeb
Caeden117 <caeden.s@outlook.com>
2022-09-21 00:30:34 -0700

    Follow LiteNetLib's example

--- ChroMapTogether/UDP/UDPServer.cs
+++ ChroMapTogether/UDP/UDPServer.cs
@@ -1,31 +1,40 @@
-﻿using ChroMapTogether.Registries;
+﻿using ChroMapTogether.Configuration;
+using ChroMapTogether.Registries;
 using LiteNetLib;
 using LiteNetLib.Utils;
+using Microsoft.Extensions.Options;
 using Serilog;
 using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Timers;
 
 namespace ChroMapTogether.UDP
 {
-    public sealed class UDPServer : IDisposable, INetLogger
+    public sealed class UDPServer : IDisposable, INetLogger, INatPunchListener
     {
         private readonly NetManager netManager;
         private readonly EventBasedNetListener eventBasedNetListener;
         private readonly ILogger logger;
         private readonly ServerRegistry serverRegistry;
+        private readonly IOptions<ServerConfiguration> serverConfig;
         private readonly Timer timer;
+        private readonly Dictionary<string, Host> hosts = new();
 
-        public UDPServer(ILogger logger, ServerRegistry serverRegistry)
+        public UDPServer(ILogger logger, ServerRegistry serverRegistry, IOptions<ServerConfiguration> serverConfig)
         {
             this.logger = logger.ForContext<UDPServer>();
             this.serverRegistry = serverRegistry;
+            this.serverConfig = serverConfig;
 
-            eventBasedNetListener = new EventBasedNetListener();
+            eventBasedNetListener = new();
             eventBasedNetListener.ConnectionRequestEvent += EventBasedNetListener_ConnectionRequestEvent;
 
             NetDebug.Logger = this;
 
             netManager = new NetManager(eventBasedNetListener);
+            netManager.NatPunchEnabled = true;
+            netManager.NatPunchModule.Init(this);
             netManager.StartInManualMode(6969);
 
             timer = new Timer(1 / 60d * 1000);
@@ -48,22 +57,14 @@ namespace ChroMapTogether.UDP
         {
             logger.Information("Got an incoming UDP connection");
 
-            if (request.Data.TryGetString(out var serverGuid) && Guid.TryParse(serverGuid, out var guid))
+            if (request.Data.TryGetString(out var roomCode) && roomCode.Length == serverConfig.Value.RoomCodeLength)
             {
-                var session = serverRegistry.GetServer(guid);
-
+                var session = serverRegistry.GetServer(roomCode);
+                
                 if (session != null)
                 {
-                    var peer = request.Accept();
-
-                    logger.Information($"RREP Address: {request.RemoteEndPoint.Address.MapToIPv4()}");
-                    logger.Information($"RREP Port: {request.RemoteEndPoint.Port}");
-                    logger.Information($"Peer Address: {peer.EndPoint.Address.MapToIPv4()}");
-                    logger.Information($"Peer Port: {peer.EndPoint.Port}");
-
-                    session.port = peer.EndPoint.Port;
-
-                    logger.Information("Successfully established UDP connection with a host.");
+                    request.Accept();
+                    logger.Information("Successfully established UDP connection.");
                     return;
                 }
             }
@@ -73,9 +74,39 @@ namespace ChroMapTogether.UDP
             request.Reject();
         }
 
-        public void WriteNet(NetLogLevel level, string str, params object[] args)
+        public void OnNatIntroductionRequest(IPEndPoint localEndPoint, IPEndPoint remoteEndPoint, string roomCode)
         {
-            logger.Information(str, args);
+            var server = serverRegistry.GetServer(roomCode);
+
+            if (server == null)
+            {
+                logger.Information("Attempted NAT hole punching for session that no longer exists.");
+
+                if (hosts.ContainsKey(roomCode))
+                {
+                    hosts.Remove(roomCode);
+                }
+                return;
+            }
+
+            if (hosts.TryGetValue(roomCode, out var host))
+            {
+                netManager.NatPunchModule.NatIntroduce(
+                    host.InternalAddr, host.ExternalAddr, localEndPoint, remoteEndPoint, roomCode);
+            }
+            else
+            {
+                server.ip = remoteEndPoint.Address.MapToIPv4().ToString();
+                server.port = remoteEndPoint.Port;
+                hosts[roomCode] = new(localEndPoint, remoteEndPoint, roomCode);
+            }
         }
+
+        // Do nothing: We are server
+        public void OnNatIntroductionSuccess(IPEndPoint targetEndPoint, NatAddressType type, string token) { }
+
+        public void WriteNet(NetLogLevel level, string str, params object[] args) => logger.Information(str, args);
+
+        public record Host(IPEndPoint InternalAddr, IPEndPoint ExternalAddr, string roomCode);
     }
 }
