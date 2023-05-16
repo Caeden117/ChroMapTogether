1dfe2f708c9a79740a9fc78a18f4d6585a58d227
Caeden117 <caeden.s@outlook.com>
2023-05-16 10:42:18 -0700

    Remove amibigous 0/O from session code provider

--- ChroMapTogether/Providers/SessionCodeProvider.cs
+++ ChroMapTogether/Providers/SessionCodeProvider.cs
@@ -4,19 +4,11 @@ namespace ChroMapTogether.Providers
 {
     public class SessionCodeProvider
     {
-        private static readonly string _alphanumeric = "ABCDEFGHJKLMNPQRSTUVWXYZ012345789";
-
-        private readonly RNGCryptoServiceProvider _rngCryptoServiceProvider;
-
-        public SessionCodeProvider(RNGCryptoServiceProvider rngCryptoServiceProvider)
-        {
-            _rngCryptoServiceProvider = rngCryptoServiceProvider;
-        }
+        private static readonly string _alphanumeric = "ABCDEFGHJKLMNPQRSTUVWXYZ12345789";
 
         public string Generate(int length = 5)
         {
-            var randomBytes = new byte[length];
-            _rngCryptoServiceProvider.GetBytes(randomBytes);
+            var randomBytes = RandomNumberGenerator.GetBytes(length);
             return string.Create(length, randomBytes, (str, randomBytes) => {
                 for (var i = 0; i < str.Length; i++)
                 {
